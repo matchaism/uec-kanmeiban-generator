473278cde6960d59b1f6a4e9a22f933de69e5b41
matchanism@Reina <60777705+macchanism@users.noreply.github.com>
2022-03-04 14:34:30 +0900

    subdivide and update, modify

--- js/index.js
+++ js/index.js
@@ -20,30 +20,3 @@ async function generateCanvas(){
 
   return canvas;
 }
-
-function GenAndDL(){
-  previewer(); // Generator refers the latest previewed content.
-  generateCanvas().then(function(canvas){
-    // Generate and click a virtual a-tag content to download.
-    let dl = document.createElement('a');
-    dl.href = canvas.toDataURL();
-    dl.download = "ueckanmeiban.png";
-    dl.click();
-  }).catch(function(err){
-    alert("Failed to generate...");
-  });
-}
-
-function GenAndOpen(){
-  previewer(); // Generator refers the latest previewed content.
-  generateCanvas().then(function(canvas){
-    // Generate and click a virtual a-tag content to open.
-    let opennewtab = document.createElement('a');
-    opennewtab.rel = "noopener noreferrer";
-    opennewtab.target = "_blank";
-    opennewtab.href = canvas.toDataURL();
-    opennewtab.click();
-  }).catch(function(err){
-    alert("Failed to generate...");
-  });
-}
